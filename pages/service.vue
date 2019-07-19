29242229694909cc11771185c092227fdb4d0e05
GhostCat <1169518718@qq.com>
2019-07-19 09:37:14 +0800

    修改客服页面

--- pages/service.vue
+++ pages/service.vue
@@ -8,10 +8,10 @@
           <div>
             <span class="left">选择平台：</span>
             <div class="platform">
-              <input type="radio" checked v-model="field.type" value="0" name="phone" id="androld" />
+              <input type="radio" checked v-model="platform.type" value="0" name="phone" id="androld" />
               <label class="active" for="androld">安卓手机</label>
 
-              <input type="radio" v-model="field.type" value="1" name="phone" id="ios" />
+              <input type="radio" v-model="platform.type" value="1" name="phone" id="ios" />
               <label for="ios">苹果手机</label>
             </div>
           </div>
@@ -53,48 +53,50 @@
 </template>
 
 <script>
+import axios from "axios"
 export default {
   data() {
     return {
       suggest: "", //建议
       number: "", // 联系方式
-      field: {
-        type: "0"
-      }
+      platform: { 
+        type: "0" // 0安卓 1苹果
+      },
+      images:''
     };
   },
   methods: {
-    // 切换安卓/苹果
-    toggle() {},
-    // 发送
-    submitForm() {
-      console.log("test");
+    // 发送数据
+    submitForm () {
       var formData = {
         suggest: this.suggest,
         number: this.number,
-        phoneType: this.field.type
+        phoneType: this.platform.type,
+        images: this.images
       };
       console.log(formData);
+
     },
-    uploadImg: (e, type) => {
+    uploadImg (e, type) {
       let file = e.target.files[0];
       console.log(file);
       if (!/\.(gif|jpg|jpeg|png|GIF|JPG|PNG|JPEG)$/.test(file.name)) {
-        this.alert(this.l.ST_TEXT5);
+        this.alert(this.l.ST_TEXT5)
         return;
       }
       let param = new FormData(); //创建form对象
       param.append("file", file); //通过append向form对象添加数据
-      console.log(param, param.get("file"));
-
-      axios
-        .post("http://localhost:3000/upload", param)
-        .then(function(response) {
-          console.log(response);
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
+      // console.log(param, param.get("file"));
+      // console.log(this)
+      this.images = param
+      // axios
+      //   .post("http://localhost:3000/upload", param)
+      //   .then(function(response) {
+      //     console.log(response);
+      //   })
+      //   .catch(function(error) {
+      //     console.log(error);
+      //   });
     }
   }
 };
@@ -103,140 +105,142 @@ export default {
 <style lang="scss" scoped>
 @import "../assets/css/varibale"; // 引入全局样式
 
-.content {
-  // background-color: $color;
-  background: linear-gradient(to right, #ff5589, #ff77a0);
-  padding-top: 3rem;
-  height: 100%;
-  position: relative;
-  &::after,
-  &::before {
-    content: "";
-    display: inline-block;
-    background: url("../assets/img/ellipse.png") no-repeat;
-    background-size: 100% 100%;
-    width: 15em;
-    height: 15em;
-    position: absolute;
-  }
-  &::after {
-    background-position: 6em 4em;
-    bottom: 0em;
-    right: 0em;
-    width: 20em;
-    height: 20em;
-  }
-  &::before {
-    background-position: -4em -2em;
-    top: 0em;
-    left: 0em;
-  }
-  .title {
-    color: #fff;
-    font-size: 2rem;
-    text-align: center;
-    margin: 0 0 2rem;
-    letter-spacing: 0.2rem;
-  }
-  .main {
-    width: 60%;
-    margin: 0 auto 6rem;
-    background-color: #fff;
-    padding: 2.5rem 5rem;
-    // box-shadow: 0px 0px 10px 2px #bbb;
-    // box-shadow: 0px 0px 10px 2px #bbb;
-    .m-title {
-      text-align: center;
-      font-size: 1.5rem;
-      color: $color;
-      margin-bottom: 2.5em;
-    }
-    .from {
-      width: 100%;
-      & > div {
-        width: 100%;
-        display: flex;
-        margin-bottom: 0.6em;
-        & > .left {
-          padding: 0.2em 1em;
-          font-size: 0.9em;
-        }
-        & > div {
-          flex: 1;
-          vertical-align: top;
-          textarea {
-            width: 100%;
-            height: 12em;
-            padding: 0.6em 1em;
-            line-height: 1.5em;
-            font-size: 0.8em;
-            color: #333;
-            border: 1px solid #d7d7d7;
-          }
-          input {
-            border: 1px solid #d7d7d7;
-          }
-        }
-
-        .seimg {
-          label {
-            border: 1px solid #d7d7d7;
-            padding: 0.4em 1em;
-            font-size: 0.8em;
-            cursor: pointer;
-          }
-          input {
-            opacity: 0;
-            display: none;
-          }
-          span {
-            margin-left: 0.3em;
-            font-size: 0.7em;
-          }
-        }
-        .phone {
-          .mark {
-            font-size: 0.7em;
-          }
-          input {
-            font-size: 0.7em;
-            padding: 0.5em;
-          }
-        }
-      }
-    }
-    .platform {
-      input {
-        display: none;
-      }
-      label {
-        display: inline-block;
-        padding: 0.3em 1em;
-        text-align: center;
-        font-size: 0.8em;
-        border-radius: 5px;
-        border: 1px solid $color;
-        margin-right: 1em;
-        cursor: pointer;
-      }
-
-      input:checked + label {
-        background-color: $color;
-        color: #fff;
-      }
-    }
-    .submit {
-      text-align: center;
-      margin: 2em auto;
-      span {
-        padding: 0.5em 3.5em;
-        background-color: $color;
-        color: #fff;
-        font-size: 1.2em;
-        border-radius: 10px;
-        cursor: pointer;
-      }
-    }
-  }
+.content{
+  margin:10em auto;
+  height: 1000px;
 }
+// .content {
+//   background: linear-gradient(to right, #ff5589, #ff77a0);
+//   padding-top: 3rem;
+//   height: 100%;
+//   position: relative;
+//   &::after,
+//   &::before {
+//     content: "";
+//     display: inline-block;
+//     background: url("../assets/img/ellipse.png") no-repeat;
+//     background-size: 100% 100%;
+//     width: 15em;
+//     height: 15em;
+//     position: absolute;
+//   }
+//   &::after {
+//     background-position: 6em 4em;
+//     bottom: 0em;
+//     right: 0em;
+//     width: 20em;
+//     height: 20em;
+//   }
+//   &::before {
+//     background-position: -4em -2em;
+//     top: 0em;
+//     left: 0em;
+//   }
+//   .title {
+//     color: #fff;
+//     font-size: 2rem;
+//     text-align: center;
+//     margin: 0 0 2rem;
+//     letter-spacing: 0.2rem;
+//   }
+//   .main {
+//     width: 60%;
+//     margin: 0 auto 6rem;
+//     background-color: #fff;
+//     padding: 2.5rem 5rem;
+//     // box-shadow: 0px 0px 10px 2px #bbb;
+//     .m-title {
+//       text-align: center;
+//       font-size: 1.5rem;
+//       color: $color;
+//       margin-bottom: 2.5em;
+//     }
+//     .from {
+//       width: 100%;
+//       & > div {
+//         width: 100%;
+//         display: flex;
+//         margin-bottom: 0.6em;
+//         & > .left {
+//           padding: 0.2em 1em;
+//           font-size: 0.9em;
+//         }
+//         & > div {
+//           flex: 1;
+//           vertical-align: top;
+//           textarea {
+//             width: 100%;
+//             height: 12em;
+//             padding: 0.6em 1em;
+//             line-height: 1.5em;
+//             font-size: 0.8em;
+//             color: #333;
+//             border: 1px solid #d7d7d7;
+//           }
+//           input {
+//             border: 1px solid #d7d7d7;
+//           }
+//         }
+
+//         .seimg {
+//           label {
+//             border: 1px solid #d7d7d7;
+//             padding: 0.4em 1em;
+//             font-size: 0.8em;
+//             cursor: pointer;
+//           }
+//           input {
+//             opacity: 0;
+//             display: none;
+//           }
+//           span {
+//             margin-left: 0.3em;
+//             font-size: 0.7em;
+//           }
+//         }
+//         .phone {
+//           .mark {
+//             font-size: 0.7em;
+//           }
+//           input {
+//             font-size: 0.7em;
+//             padding: 0.5em;
+//           }
+//         }
+//       }
+//     }
+//     .platform {
+//       input {
+//         display: none;
+//       }
+//       label {
+//         display: inline-block;
+//         padding: 0.3em 1em;
+//         text-align: center;
+//         font-size: 0.8em;
+//         border-radius: 5px;
+//         border: 1px solid $color;
+//         margin-right: 1em;
+//         cursor: pointer;
+//       }
+
+//       input:checked + label {
+//         background-color: $color;
+//         color: #fff;
+//       }
+//     }
+//     .submit {
+//       text-align: center;
+//       margin: 2em auto;
+//       span {
+//         padding: 0.5em 3.5em;
+//         background-color: $color;
+//         color: #fff;
+//         font-size: 1.2em;
+//         border-radius: 10px;
+//         cursor: pointer;
+//       }
+//     }
+//   }
+// }
 </style>
