4d97274344b77c9a8a9b6f1d3fad88ceff43b647
GhostCat <1169518718@qq.com>
2019-08-22 23:55:37 +0800

    Markdown 后台发布编辑

---
+++ pages/edit.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Markdown 后台发布</h1>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    
+h1{
+    text-align: center;
+}
+
+</style>
+
+<script>
+export default {
+    
+}
+</script>
