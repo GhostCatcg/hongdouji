379c9cd58a0a3647fa75a0ef1fa417759ac04060
GhostCat <1169518718@qq.com>
2019-09-10 22:35:30 +0800

    路由更新

---
+++ pages/news/_id.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>哈哈哈</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+    validate ({ params }) {
+    // 必须是number类型
+    console.log(params)
+    return /^\d+$/.test(params.id)
+  }
+};  
+</script>
